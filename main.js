6daa148ceb6508317925180dcefcf072044c19b1
NotSault <rosiquevictor@gmail.com>
2023-12-22 13:04:36 +0100

    update

--- main.js
+++ main.js
@@ -1,76 +1,85 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const { autoUpdater } = require('electron-updater');
+const path = require('path');
+const log = require('electron-log');
+
+// Configurez le logging pour electron-updater
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
 
 let mainWindow;
-let updateWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 1920,
-        height: 1080,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
+  // Créez la fenêtre du navigateur.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, 
+      enableRemoteModule: true 
+    }
+  });
 
-    mainWindow.loadFile('web\index.html');
+  // Chargez le fichier index.html de l'application.
+  mainWindow.loadFile('index.html');
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  // Ouvrez les DevTools. Commentez ceci avant de déployer la version finale.
+  // mainWindow.webContents.openDevTools();
 }
 
-function createUpdateWindow() {
-    updateWindow = new BrowserWindow({
-        width: 400,
-        height: 200,
-        frame: false,
-        transparent: true,
-        alwaysOnTop: true,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
+// Cette méthode sera appelée quand Electron aura fini
+// de s'initialiser et sera prêt à créer des fenêtres de navigation.
+app.whenReady().then(() => {
+  createWindow();
 
-    updateWindow.loadFile('update.html');
+  app.on('activate', () => {
+    // Sur macOS, il est courant de recréer une fenêtre dans l'application quand
+    // l'icône du dock est cliquée et qu'il n'y a pas d'autres fenêtres d'ouvertes.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 
-    updateWindow.on('closed', () => {
-        updateWindow = null;
-    });
-}
-
-app.whenReady().then(createWindow);
+  // Vérifiez les mises à jour après la création de la fenêtre
+  autoUpdater.checkForUpdatesAndNotify();
+});
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  // Quittez l'application lorsque toutes les fenêtres sont fermées, sauf sur macOS.
+  if (process.platform !== 'darwin') app.quit();
 });
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+// Logique de mise à jour automatique
+autoUpdater.on('checking-for-update', () => {
+  log.info('Recherche de mises à jour...');
 });
 
-app.on('ready', () => {
-    autoUpdater.checkForUpdatesAndNotify();
+autoUpdater.on('update-available', (info) => {
+  log.info('Une mise à jour est disponible.');
+  mainWindow.webContents.send('update_available');
 });
 
-autoUpdater.on('update-available', () => {
-    createUpdateWindow();
+autoUpdater.on('update-not-available', (info) => {
+  log.info('Aucune mise à jour disponible.');
+  mainWindow.webContents.send('update_not_available');
 });
 
-autoUpdater.on('update-downloaded', () => {
-    if (updateWindow) {
-        updateWindow.close();
-    }
-    autoUpdater.quitAndInstall();
+autoUpdater.on('error', (err) => {
+  log.error('Erreur dans auto-updater: ' + err);
+  mainWindow.webContents.send('update_error', err);
 });
 
-// Ici, vous pouvez gérer d'autres messages IPC si nécessaire
-ipcMain.on('start-update', () => {
-    autoUpdater.quitAndInstall();
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = `Vitesse de téléchargement: ${progressObj.bytesPerSecond}`;
+  log_message += ` - Téléchargé ${progressObj.percent}%`;
+  log_message += ` (${progressObj.transferred}/${progressObj.total})`;
+  log.info(log_message);
+  mainWindow.webContents.send('update_download_progress', progressObj);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  log.info('Mise à jour téléchargée; elle sera installée dans 5 secondes.');
+  mainWindow.webContents.send('update_downloaded');
+  setTimeout(() => {
+    autoUpdater.quitAndInstall();  
+  }, 5000);
 });
